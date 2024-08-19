74dae89c1c8b7ff9b537bf240bf6a25c5b518f4c
Eminem <58310848+thebadcoder96@users.noreply.github.com>
2024-08-19 11:58:43 -0500

    fixed scrapping for amazon, needs more test

--- src/scrape.py
+++ src/scrape.py
@@ -2,11 +2,27 @@ import aiohttp
 
 from bs4 import BeautifulSoup
 
+
+default_header = {
+    "User-Agent": "LegalEaseUserAgent",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Referer": "https://www.google.com/",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+}
+
+
 async def fetch(url: str) -> str:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+        async with session.get(
+            url,
+            headers=default_header,
+            ssl=None,
+        ) as response:
             if response.status != 200:
-                return f'Failed to fetch {url}: {response.status}'
+                raise Exception(f'Failed to fetch {url}: {response.status}')
             return await response.text()
         
 async def scrape(url: str) -> str:
